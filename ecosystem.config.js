2063f120f71979434656f248aa85bf1e9c970be0
techkastudios <hellotechkastudios@gmail.com>
2026-02-13 07:31:39 +0600

    Modify FE app settings for production deployment
    Updated application configuration for production environment.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,26 +1,19 @@
 module.exports = {
   apps: [
     {
-      name: 'FE',
-      exec_mode: 'cluster',
-      instances: 'max', // Or a number of instances
-      script: './node_modules/nuxt/bin/nuxt.js',
-      args: 'start',
+      name: "FE",
+      cwd: "/home/forge/staging.thefinancialexpress.com.bd/current",
+      exec_mode: "fork",
+      instances: 1,
+      script: "./node_modules/nuxt/bin/nuxt.js",
+      args: "start",
       autorestart: true,
       env: {
-        PORT: 3000
+        NODE_ENV: "production",
+        HOST: "127.0.0.1",
+        PORT: "3000",
+        NODE_OPTIONS: "--max_old_space_size=2048"
       }
-    },
-    {
-      name: 'FEDev',
-      exec_mode: 'fork',
-      instances: 1, // Or a number of instances
-      script: './node_modules/nuxt/bin/nuxt.js',
-      args: 'start',
-      autorestart: true,
-      env: {
-        PORT: 4000
-      }
-    },
-  ],
+    }
+  ]
 };
