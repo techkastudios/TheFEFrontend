1c25edd419681bc2c84a9f852cbc75c25a9f122c
techkastudios <hellotechkastudios@gmail.com>
2026-02-13 07:32:31 +0600

    Change 'FE' app to cluster mode with max instances
    Updated application configuration to use cluster mode and maximum instances for the 'FE' app.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,19 +1,26 @@
 module.exports = {
   apps: [
     {
-      name: "FE",
-      cwd: "/home/forge/staging.thefinancialexpress.com.bd/current",
-      exec_mode: "fork",
-      instances: 1,
-      script: "./node_modules/nuxt/bin/nuxt.js",
-      args: "start",
+      name: 'FE',
+      exec_mode: 'cluster',
+      instances: 'max', // Or a number of instances
+      script: './node_modules/nuxt/bin/nuxt.js',
+      args: 'start',
       autorestart: true,
       env: {
-        NODE_ENV: "production",
-        HOST: "127.0.0.1",
-        PORT: "3000",
-        NODE_OPTIONS: "--max_old_space_size=2048"
+        PORT: 3000
       }
-    }
-  ]
+    },
+    {
+      name: 'FEDev',
+      exec_mode: 'fork',
+      instances: 1, // Or a number of instances
+      script: './node_modules/nuxt/bin/nuxt.js',
+      args: 'start',
+      autorestart: true,
+      env: {
+        PORT: 4000
+      }
+    },
+  ],
 };
